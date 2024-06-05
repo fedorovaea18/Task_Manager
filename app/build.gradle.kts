2ded31cf6a1c5e3851cc75b19b68fe5dc75840fb
katerina <fedorovaea18@yandex.ru>
2024-06-05 23:06:21 +0300

    step 1

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    application
+    checkstyle
+    jacoco
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.3"
+}
+
+application {
+    mainClass.set("hexlet.code.AppApplication")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required = true
+    }
+}
