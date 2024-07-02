48e959693d28b71c20b82a415991b4c9e61e3017
katerina <fedorovaea18@yandex.ru>
2024-07-02 16:10:07 +0300

    fix LabelControllerTest

--- app/src/main/java/hexlet/code/dto/labels/LabelCreateDTO.java
+++ app/src/main/java/hexlet/code/dto/labels/LabelCreateDTO.java
@@ -2,11 +2,15 @@ package hexlet.code.dto.labels;
 
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LabelCreateDTO {
 
     @Column(unique = true)
